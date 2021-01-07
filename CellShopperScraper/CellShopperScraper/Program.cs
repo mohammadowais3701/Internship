eda32cb06da8c817349ee32947b2e820b74d9770
Muhammad Owais <mohammadowais3701@gmail.com>
2021-01-07 20:31:15 +0500

    Day 13 Cell shopper

--- CellShopperScraper/CellShopperScraper/Program.cs
+++ CellShopperScraper/CellShopperScraper/Program.cs
@@ -15,313 +15,381 @@ namespace CellShopperScraper
 {
     class Program
     {
-      
-       static StreamWriter w;
-       
 
-   static void Main(string[] args)
+        static StreamWriter w;
+        private static long counter;
+        static List<ProductDetails> myproducts=new List<ProductDetails>();
+
+
+        static void Main(string[] args)
         {
-           
-          //  cellShopper("http://cellshopper.com");
-            makeWebRequest("http://cellshopper.com");
-            
+
+            cellShopper("http://cellshopper.com");
+            //          makeWebRequest("http://cellshopper.com");
+
+
         }
 
 
 
 
-   static Stream makeWebRequest(string web) {
+        static Stream makeWebRequest(string web)
+        {
             Stream data;
-            HttpWebRequest myreq = (HttpWebRequest)WebRequest.CreateHttp(web);
+            Uri uri = new Uri(web);
+            Console.WriteLine(uri);
+            HttpWebRequest myreq = (HttpWebRequest)WebRequest.CreateHttp(uri);
+            WebProxy proxy = new WebProxy("127.0.0.1", 8888);
+            myreq.Proxy = proxy;
 
             myreq.Method = "GET";
-        //  myreq.AllowAutoRedirect = false;
-            
+            myreq.AllowAutoRedirect = false;
+
+
             HttpWebResponse myres = (HttpWebResponse)myreq.GetResponse();
+
             Console.WriteLine(myres.Headers["location"]);
-            if (myres.Headers["location"]== null)
+
+
+
+
+            if (myres.Headers["location"] == null)
             {
+
                 data = myres.GetResponseStream();
                 return data;
             }
-            //   Console.WriteLine(myres.Headers["location"]);
 
-            else  {
+
+            else
+            {
 
                 if (myres.Headers["location"].StartsWith(".."))
                 {
-                    string[] str = web.Split('/');
-                    
+                    // Console.WriteLine(myres.Headers["location"]);
+                    string loc = "", cur_str = "";
+
+                    for (int i = 0; i < uri.Segments.Length - 2; i++)
+                    {
+
+                        cur_str += uri.Segments[i];
+                    }
+                    cur_str += myres.Headers["location"].Replace("../", "");
+                    loc = "http://" + uri.Host;
+
+
+
+                    data = makeWebRequest(loc + cur_str);
+                    return data;
 
                 }
                 else
                 {
+
                     data = makeWebRequest(web + "/" + myres.Headers["location"]);
                     return data;
                 }
             }
-          
-            
 
-        
+
+
+
         }
 
-  static void cellShopper(string s) {
-      Stream data;
-      Regex reg;
-      MatchCollection mt;
-     
-      try
-      {
-          w = new StreamWriter("output.csv");
-         
-
-              var newLine = string.Format("PRODUCT NAME,ITEM CODE,ITEM ID,WEIGHT,PRICE,CATEGORY");
-
-              w.WriteLine(newLine);
-              w.Flush();
-
-
-          
-          data = makeWebRequest(s);
-          var page = new HtmlDocument();
-         
-          page.Load(data);
-          //
-          if (page.DocumentNode.SelectNodes("//ul[1]/li/a") != null)
-          {
-              HtmlNodeCollection nodes = page.DocumentNode.SelectNodes("//ul[1]/li/a");
-              foreach (var n in nodes)
-              {
-               
-                  reg = new Regex("home.php.*\\d");
-                  mt = reg.Matches(n.Attributes["href"].Value);
-                  foreach (var m in mt)
-                  {
-
-
-       //new Thread(() => InternalLink("http://cellshopper.com/store/" + m)).Start();
-          InternalLink("http://cellshopper.com/store/" + m);
-                  }
-
-
-              }
-
-          }
-
-
-          data.Close();
-    
-      }
-      catch (Exception ex)
-      {
-          Console.WriteLine("In Cell shopper");
-          Console.WriteLine(ex.Message);
-        
-      }
-
-      
-
-            
-            
-        
-        }
-  static void InternalLink(string s) {
-
-      Stream data;
-      try
-      {
-          data = makeWebRequest(s);
-      
-          var page = new HtmlDocument();
-        
-          page.Load(data);
-
-      if (page.DocumentNode.SelectNodes("//div[@id='content']/div[@id='phonelist']/a[1]")!=null)
-          {
-
-              HtmlNodeCollection nodes = page.DocumentNode.SelectNodes("//div[@id='content']/div[@id='phonelist']/a[1]");
-              foreach (var n in nodes)
-              {
-                  string att = (n.Attributes["href"].Value).Replace("amp;", "");
-                  Console.WriteLine("http://cellshopper.com/store/" + att);
-
-                  InternalLink("http://cellshopper.com/store/" + att);
-
-              }
-          }
-          else if (page.DocumentNode.SelectNodes("//div[@id='content']//div[@id='products_list']/a[1]") != null)
-          {
-              HtmlNodeCollection nodes = page.DocumentNode.SelectNodes("//div[@id='content']//div[@id='products_list']/a[1]");
-              foreach (var n in nodes)
-              {
-                  string att = (n.Attributes["href"].Value).Replace("amp;", "");
-
-                  Console.WriteLine("here2");
-                  Console.WriteLine("http://cellshopper.com/store/" + att);
-                  InternalLink("http://cellshopper.com/store/" + att);
-
-              }
-          }
-      else if (page.DocumentNode.SelectNodes("//div[@id='product_page_details']") != null)
-      {
-          InternalLink3(s);
-
-      }
-      else {
-          return;
-      
-      }
-      data.Close();
-   
-      }
-      catch (Exception ex) {
-          Console.WriteLine("IN Internal Link");
-          Console.WriteLine(ex.Message);
-      
-      }
-
- 
- 
-      }
-
-static void InternalLink3(string s) {
-    Stream data;
-    String str;
-   
-    try
-    {
-        data = makeWebRequest(s);
-
-        var page = new HtmlDocument();
-   
-        page.Load(data);
-
-       
-        HtmlNodeCollection nodes = page.DocumentNode.SelectNodes("//div[@id='product_page_details']");
-        if (nodes == null)
+        static void cellShopper(string s)
         {
-            return;
-
-        }
-        Regex reg;
-        MatchCollection mt;
-        ProductDetails obj=new ProductDetails();
-      lock(obj){  
-        foreach (var n in nodes)
-        {
-            
-             reg = new Regex("<h2 style=.*>");
-             mt = reg.Matches(n.InnerHtml);
-             foreach(var k in mt){
-                 obj.productName =Convert.ToString(k);
-             }
-             Console.WriteLine();
-             obj.productName = obj.productName.Split('>')[1].Split('<')[0].Replace(",","|");
-             reg = new Regex("Item Code:.*<");
-             mt = reg.Matches(n.InnerHtml);
-             foreach (var k in mt)
-             {
-                 obj.itemCode = Convert.ToString(k);
-             }
-             obj.itemCode = obj.itemCode.Split('<')[0].Split(':')[1].Trim().Replace("&nbsp;", "");
-
-            // itemcode= n.InnerText.Split('\n')[4].Split(':')[1];
-
-             reg = new Regex("Item ID:.*<");
-             mt = reg.Matches(n.InnerHtml);
-             foreach (var k in mt)
-             {
-                 obj.itemId = Convert.ToString(k);
-             }
-             obj.itemId = obj.itemId.Split('<')[0].Split(':')[1].Replace("&nbsp;","");
-            // itemId=n.InnerText.Split('\n')[5].Split(':')[1].Replace("&nbsp;","");
-             reg = new Regex("Weight.*<");
-             mt = reg.Matches(n.InnerHtml);
-             foreach (var k in mt)
-             {
-                obj.weight= Convert.ToString(k);
-             }
-             obj.weight = obj.weight.Split('<')[0].Split(':')[1].Replace("&nbsp;", "");
-            // weight=n.InnerText.Split('\n')[6].Split(':')[1].Replace("&nbsp;", "");
-
-             reg = new Regex("Price:.*<");
-             mt = reg.Matches(n.InnerHtml);
-             foreach (var k in mt)
-             {
-                 obj.price = Convert.ToString(k);
-             }
-             string[] prices = obj.price.Split(':')[1].Replace("&nbsp;", "").Replace("<s>", "").Replace("</s>", ",").Replace("<", "").Split(',');
-             if (prices.Length == 2)
-                obj.price = prices[1];
-             else 
-                obj.price=prices[0];
-             
-             //price=n.InnerText.Split('\n')[7].Split(':')[1].Replace("&nbsp;", "");
-          
-
-            
-           // Console.WriteLine("http://cellshopper.com/store/" + n.Attributes["href"].Value);
-
-        }
-
-        obj.cat = "";
-            nodes = page.DocumentNode.SelectNodes("//div[@id='bread-crumb']/a");
-            if (nodes == null)
-            {
-                return;
-
-            }
-            foreach (var n in nodes) {
-                obj.cat += n.InnerText+":";
-            }
-            obj.cat=obj.cat.Remove(obj.cat.Length - 1);
-
-            var newLine = string.Format("{0},{1},{2},{3},{4},{5}", obj.productName, obj.itemCode, obj.itemId, obj.weight, obj.price, obj.cat);
-
+            Stream data;
            
-            w.WriteLine(newLine);
-                //File.AppendAllText("output.csv", newLine);
-            w.Flush();
 
-            
-            
-            nodes = page.DocumentNode.SelectNodes("//a[@class='thickbox']");
-            if (nodes == null)
+            try
             {
-                return;
+                w = new StreamWriter("output.csv");
+
+
+                var newLine = string.Format("PRODUCT NAME,ITEM CODE,ITEM ID,WEIGHT,PRICE,CATEGORY");
+
+                w.WriteLine(newLine);
+                w.Flush();
+
+                w.Close();
+
+                data = makeWebRequest(s);
+                var page = new HtmlDocument();
+
+                page.Load(data);
+              
+              if( page.DocumentNode.SelectSingleNode("//div[@id='primarynav']/ul[1]/li/a")!=null)
+
+                {
+
+                    HtmlNodeCollection nodes = page.DocumentNode.SelectNodes("//div[@id='primarynav']/ul[1]/li/a");
+                    foreach (var n in nodes)
+                    {
+                      //  Console.WriteLine(n.Attributes["href"].Value);
+
+
+
+                        Thread t1 = new Thread(() => InternalLink("http://cellshopper.com/store/" + n.Attributes["href"].Value));
+                            t1.Start();
+
+                        //InternalLink("http://cellshopper.com/store/" + n.Attributes["href"].Value);
+                      
+
+
+                    }
+
+                }
+
+
+                data.Close();
 
             }
-            Directory.CreateDirectory(obj.itemCode);
-             string localFilename = obj.itemCode;
-             int i=1;
+            catch (Exception ex)
+            {
+                Console.WriteLine("In Cell shopper");
+                Console.WriteLine(ex.Message);
+
+            }
+
+
+
+
+
+
+        }
+
+
+        static void InternalLink(string s)
+        {
+
+            Stream data;
+            try
+            {
+                data = makeWebRequest(s);
+
+                var page = new HtmlDocument();
+
+                page.Load(data);
+
+                if (page.DocumentNode.SelectNodes("//div[@id='content']/div[@id='phonelist']/a") != null)
+                {
+
+
+                    HtmlNodeCollection nodes = page.DocumentNode.SelectNodes("//div[@id='content']/div[@id='phonelist']/a[not(.//img)]");
+                    phoneList_productList(nodes);
+
+                }
+                else if (page.DocumentNode.SelectNodes("//div[@id='content']//div[@id='products_list']/a") != null)
+                {
+
+                    HtmlNodeCollection nodes = page.DocumentNode.SelectNodes("//div[@id='content']//div[@id='products_list']/a");
+                    phoneList_productList(nodes);
+
+                }
+                else if (page.DocumentNode.SelectNodes("//div[@id='product_page_details']") != null)
+                {
+
+
+                    InternalLink3(s);
+
+                }
+                else
+                {
+                    return;
+
+                }
+                data.Close();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("IN Internal Link");
+                Console.WriteLine(ex.Message);
+
+            }
+
+
+
+        }
+      static  Object myobj = new object();
+        static void InternalLink3(string s)
+        {
+
+            Stream data;
+            String str;
+
+            try
+            {
+                data = makeWebRequest(s);
+
+                var page = new HtmlDocument();
+
+                page.Load(data);
+
+
+                HtmlNodeCollection nodes = page.DocumentNode.SelectNodes("//div[@id='product_page_details']");
+                if (nodes == null)
+                {
+                    return;
+
+                }
+                
+                
+
+
+                ProductDetails obj = new ProductDetails();
+
+                //foreach (var n in nodes)
+                {
+                    MatchCollection mt = Regex.Matches(page.DocumentNode.InnerHtml, "<h2 style=.*>");
+                    foreach (var k in mt)
+                    {
+                        obj.productName = Convert.ToString(k);
+                    }
+                    Console.WriteLine();
+                    obj.productName = obj.productName.Split('>')[1].Split('<')[0].Replace(",","");
+
+                    mt = Regex.Matches(page.DocumentNode.InnerHtml, "Item Code:.*<");
+                    foreach (Match k in mt)
+                    {
+                        obj.itemCode = k.Value;
+                    }
+                    obj.itemCode = obj.itemCode.Split('<')[0].Split(':')[1].Trim().Replace("&nbsp;", "");
+
+                    mt = Regex.Matches(page.DocumentNode.InnerHtml, "Item ID:.*<");
+                    foreach (Match k in mt)
+                    {
+                        obj.itemId = k.Value;
+                    }
+                    obj.itemId = obj.itemId.Split('<')[0].Split(':')[1].Replace("&nbsp;", "");
+
+                    mt = Regex.Matches(page.DocumentNode.InnerHtml, "Weight.*<");
+                    foreach (Match k in mt)
+                    {
+                        obj.weight = k.Value;
+                    }
+                    obj.weight = obj.weight.Split('<')[0].Split(':')[1].Replace("&nbsp;", "");
+
+                    mt = Regex.Matches(page.DocumentNode.InnerHtml, "Price:.*<");
+
+                    foreach (Match k in mt)
+                    {
+                        obj.price = k.Value;
+                    }
+                    string[] prices = obj.price.Split(':')[1].Replace("&nbsp;", "").Replace("<s>", "").Replace("</s>", ",").Replace("<", "").Split(',');
+                    if (prices.Length == 2)
+                        obj.price = prices[1];
+                    else
+                        obj.price = prices[0];
+
+                    //price=n.InnerText.Split('\n')[7].Split(':')[1].Replace("&nbsp;", "");
+
+
+
+                    // Console.WriteLine("http://cellshopper.com/store/" + n.Attributes["href"].Value);
+
+                }
+
+                obj.cat = "";
+                nodes = page.DocumentNode.SelectNodes("//div[@id='bread-crumb']/a");
+                if (nodes == null)
+                {
+                    return;
+
+                }
+                foreach (var n in nodes)
+                {
+                    obj.cat += n.InnerText + ":";
+                }
+                obj.cat = obj.cat.Remove(obj.cat.Length - 1);
+
+                Interlocked.Increment(ref counter);
+
+                var newLine = string.Format("{0},{1},{2},{3},{4},{5}", obj.productName, obj.itemCode, obj.itemId, obj.weight, obj.price, obj.cat);
+                lock (myobj)
+                {   int check=0;
+                foreach (ProductDetails product in myproducts)
+                {
+                    if (product.itemId.Equals(obj.itemId))
+                    {
+                        check = 1;
+                        break;
+                    }
+                    if (check == 0) {
+                        myproducts.Add(obj);
+                    
+                    }
+                }
+                    
+
+                   // File.AppendAllLines("output.csv", new String[] { newLine });
+
+                }
+
+                Console.Title = counter.ToString();
+
+                nodes = page.DocumentNode.SelectNodes("//a[@class='thickbox']");
+                if (nodes == null)
+                {
+                    return;
+
+                }
+                Directory.CreateDirectory(obj.itemCode);
+                string localFilename = obj.itemCode;
+                int i = 1;
+                lock (myobj)
+                {
+                    foreach (var n in nodes)
+                    {
+
+
+                        using (WebClient client = new WebClient())
+                        {
+                            client.DownloadFile(n.Attributes["href"].Value, localFilename + "/" + i + ".jpg");
+                        }
+                        i++;
+                        Console.WriteLine(n.Attributes["href"].Value);
+
+
+                    }
+                }
+
+
+                data.Close();
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("IN Internal Link3");
+                Console.WriteLine(ex.Message);
+
+            }
+
+
+
+
+        }
+
+
+        static void phoneList_productList(HtmlNodeCollection nodes)
+        {
+            string comparision = "";
             foreach (var n in nodes)
             {
-                
+                string att = (n.Attributes["href"].Value).Replace("amp;", "");
                
-                using (WebClient client = new WebClient())
-                {
-                    client.DownloadFile(n.Attributes["href"].Value, localFilename+"/"+i+".jpg");
-                }
-                i++;
-                Console.WriteLine(n.Attributes["href"].Value);
+                Console.WriteLine("http://cellshopper.com/store/" + att);
+                comparision = att;
+                InternalLink("http://cellshopper.com/store/" + att);
+
             }
-    }
-      data.Close();
-    }
-    catch (Exception ex)
-    {
-         Console.WriteLine("IN Internal Link3");
-        Console.WriteLine(ex.Message);
 
-    }
-
-
-
-}
-
-       
-
+        }
+        static void writeProducts() { 
+        
+        
+        }
 
     }
 }
