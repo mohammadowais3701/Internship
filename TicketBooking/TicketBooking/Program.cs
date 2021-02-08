0ca5f0ec3023cd0c0405ced6cd17f3059fd99766
Muhammad Owais <mohammadowais3701@gmail.com>
2021-02-08 20:52:31 +0500

    8-2-2021/axs.com

--- TicketBooking/TicketBooking/Program.cs
+++ TicketBooking/TicketBooking/Program.cs
@@ -22,7 +22,6 @@ namespace TicketBooking
             Stream data;
             try
             {
-               
                 Uri uri = new Uri(web);
                 ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12;
                 HttpWebRequest req = (HttpWebRequest)WebRequest.Create(uri);
@@ -30,23 +29,34 @@ namespace TicketBooking
             //    req.AllowAutoRedirect = false;
                 WebProxy proxy = new WebProxy("127.0.0.1", 8888);
                 req.Proxy = proxy;
-              //  req.AutomaticDecompression = DecompressionMethods.GZip;
+                req.AutomaticDecompression = DecompressionMethods.GZip;
                 req.Credentials = CredentialCache.DefaultCredentials;
                 req.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8";
-                req.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:85.0) Gecko/20100101 Firefox/85.0";
+                req.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Safari/537.36";
                 Console.WriteLine(req.Headers);
                 HttpWebResponse myres = (HttpWebResponse)req.GetResponse();
-                var reader = new System.IO.StreamReader(myres.GetResponseStream());
-                Console.WriteLine(reader.ReadToEnd());
+                var reader = new StreamReader(myres.GetResponseStream());
+              //  Console.WriteLine(reader.ReadToEnd());
+                HtmlDocument doc=new HtmlDocument();
+                doc.Load(reader);
+                try
+                {
+                   // Console.WriteLine(reader.ReadToEnd());
+                    HtmlNode node = doc.DocumentNode.SelectSingleNode("//script[@src]");
+                    Console.WriteLine(node.Attributes["src"].Value);
+                    //Console.WriteLine(req.Host);
+                   
+                  //  makeWebRequest("https://" + req.Host + node.Attributes["src"].Value);
+                }
+                catch (Exception ex) {
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine(reader.ReadToEnd());
+                }
+               
             }
             catch(Exception ex) {
                 Console.WriteLine(ex.Message);
-            
             }
-
-            
-
-        
         }
     }
 }
